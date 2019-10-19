13aeec7df7df9ae65da60a714a9c105daf5f20c4
Rickard Hultgren <sababa.sababa@gmail.com>
2019-10-19 06:48:30 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+sounds = [1.wav, 2.wav, 3.wav]
+waitperiod = 3 
+results = dict() 
+for sound in len(sounds): 
+	#Play sound 
+	Wait period 
+	If pressed during wait period: 
+		Results.append(1) 
+	Else: 
+		Results.append(0) 
+Write results into a SQLdatabase 
+Print results as PDF 
